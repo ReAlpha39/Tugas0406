6cbd83521c013b78e59dddc273ef78f2900375ff
YunusNS <yunusw718@gmail.com>
2020-04-06 10:28:20 +0800

    add ui login and create new activity for menu

---
+++ app/src/main/java/id/nyaa/exercise0406/ListBangunDatar.java
@@ -0,0 +1,14 @@
+package id.nyaa.exercise0406;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListBangunDatar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_bangun_datar);
+    }
+}
