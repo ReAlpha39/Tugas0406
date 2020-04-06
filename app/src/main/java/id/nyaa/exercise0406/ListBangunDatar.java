3eef635cf911ebbde8e8d2e73f324b4fc4153f72
YunusNS <yunusw718@gmail.com>
2020-04-06 19:26:06 +0800

    add popup menu when button clicked

--- app/src/main/java/id/nyaa/exercise0406/ListBangunDatar.java
+++ app/src/main/java/id/nyaa/exercise0406/ListBangunDatar.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.PopupMenu.OnMenuItemClickListener;
 
-public class ListBangunDatar extends AppCompatActivity {
+public class ListBangunDatar extends AppCompatActivity implements OnMenuItemClickListener {
+
+    private Button persegi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,44 @@ public class ListBangunDatar extends AppCompatActivity {
 
     public void persegiC(View view) {
 
-        startActivity(new Intent(this, ActivityRumus.class));
+        PopupMenu ppMenuP = new PopupMenu(ListBangunDatar.this, view);
+        ppMenuP.setOnMenuItemClickListener(ListBangunDatar.this);
+        ppMenuP.inflate(R.menu.popup_persegi);
+        ppMenuP.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+
+        startActivity(new Intent(this, MainActivity.class));
+        return true;
+    }
+
+    public void lingkaranC(View view) {
+        PopupMenu ppMenuP = new PopupMenu(ListBangunDatar.this, view);
+        ppMenuP.setOnMenuItemClickListener(ListBangunDatar.this);
+        ppMenuP.inflate(R.menu.popup_lingkaran);
+        ppMenuP.show();
+    }
+
+    public void persegiPC(View view) {
+        PopupMenu ppMenuP = new PopupMenu(ListBangunDatar.this, view);
+        ppMenuP.setOnMenuItemClickListener(ListBangunDatar.this);
+        ppMenuP.inflate(R.menu.popup_persegi_panjang);
+        ppMenuP.show();
+    }
+
+    public void segitigaC(View view) {
+        PopupMenu ppMenuP = new PopupMenu(ListBangunDatar.this, view);
+        ppMenuP.setOnMenuItemClickListener(ListBangunDatar.this);
+        ppMenuP.inflate(R.menu.popup_segitiga);
+        ppMenuP.show();
+    }
+
+    public void trapesiumC(View view) {
+        PopupMenu ppMenuP = new PopupMenu(ListBangunDatar.this, view);
+        ppMenuP.setOnMenuItemClickListener(ListBangunDatar.this);
+        ppMenuP.inflate(R.menu.popup_trapesium);
+        ppMenuP.show();
     }
 }
